f4cb6f55470f0872527631e10c00a8aff5c0d400
Prasad Khode <prasadkhode03@gmail.com>
2025-06-12 16:53:18 +0530

    Set Operations

---
+++ Day 9/SetOperations.py
@@ -0,0 +1,11 @@
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+#Commmon
+print("Common elements of both sets :",A.intersection(B))
+
+#Only in A
+print("Elements Only in Set A without Common's :",A - B)
+
+#Only in B
+print("Elements Only in Set B without Common's :",B - A)
