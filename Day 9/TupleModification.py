c293b06c906ad7a14e1e3c1677964bae5fa32288
Prasad Khode <prasadkhode03@gmail.com>
2025-06-13 12:46:21 +0530

    Tuple Modification Program

--- Day 9/TupleModification.py
+++ Day 9/TupleModification.py
@@ -1,4 +1,4 @@
 tup = ([1,2],[3,4])
 print("Tuple before modification : ", tup)
 tup[0][1]=99
-print("Tuple after modification : ", tup)
+print("Tuple after modification : ", tup)                                                                                    
